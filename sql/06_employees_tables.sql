0f22de65d4c99f9f67b760c222ebf0e97a3314d0
Oz <ozair.x.ahmed@gmail.com>
2026-03-17 02:07:42 -0400

    Create separate sql file for EMPLOYEES cir relationship fix

--- sql/06_employees_tables.sql
+++ sql/06_employees_tables.sql
@@ -41,23 +41,3 @@ CREATE TABLE employees.employees (
 );
 
 COMMENT ON TABLE employees.employees IS 'Employee master data, including department and salary';
-
-
-------------------------------
--- FOREIGN KEYS (circular relationship)
-------------------------------
--- Employee -> Department (many-to-one)
-ALTER TABLE employees.employees
-    ADD CONSTRAINT employees_department_id_fk
-        FOREIGN KEY (department_id)
-        REFERENCES employees.departments(department_id)
-        ON UPDATE CASCADE
-        ON DELETE SET NULL;
-
--- Department -> Employee (manager is also an employee)
-ALTER TABLE employees.departments
-    ADD CONSTRAINT departments_manager_id_fk
-        FOREIGN KEY (manager_id)
-        REFERENCES employees.employees(employee_id)
-        ON UPDATE CASCADE
-        ON DELETE SET NULL;
